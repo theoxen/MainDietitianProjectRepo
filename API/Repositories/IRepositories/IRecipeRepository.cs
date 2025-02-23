4da4a0c933be67d50c47108091ec8439c3f092a8
Pelekanos <andreas4everlol@gmail.com>
2025-02-23 22:42:41 +0200

    Updated Migrations Created ViewRecipe functionality

--- API/Repositories/IRepositories/IRecipeRepository.cs
+++ API/Repositories/IRepositories/IRecipeRepository.cs
@@ -1,7 +1,9 @@
+using API.Data;
+
 namespace API.Repositories.IRepositories
 {
     public interface IRecipeRepository
     {
-        
+        Task<Recipe?> GetRecipeByIdAsync(Guid id);
     }
 }
--- API/Repositories/RecipeRepository.cs
+++ API/Repositories/RecipeRepository.cs
@@ -1,9 +1,22 @@
+using API.Data;
 using API.Repositories.IRepositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Repositories
 {
     public class RecipeRepository : IRecipeRepository
     {
-        
+        private readonly DataContext _context;
+
+        public RecipeRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Recipe?> GetRecipeByIdAsync(Guid id)
+        {
+            return await _context.Recipes.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
     }
 }
