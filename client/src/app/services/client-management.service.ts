8d2c4ba300aaf8f097b286b374d4b01f1bd094a8
theoxen <136903152+theoxen@users.noreply.github.com>
2025-03-05 16:38:51 +0200

    added the base url in the clientmanagement service

--- client/src/app/services/client-management.service.ts
+++ client/src/app/services/client-management.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientManagementService {
-
+  private baseUrl = environment.apiUrl;
+  
   constructor(private http: HttpClient) { }
 
   GetClientIdByPhoneNumber(phoneNumber: string) {
