6ecf810198b265c7f7224a2ebb70a89f0d8a2a1b
Dimitrizk <dikaouris@gmail.com>
2025-03-05 22:06:00 +0200

    atelioto

--- client/src/app/services/client-management.service.ts
+++ client/src/app/services/client-management.service.ts
@@ -1,16 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
+import { ClientProfile } from '../models/client-management/client-profile';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientManagementService {
+ 
   private baseUrl = environment.apiUrl;
   
   constructor(private http: HttpClient) { }
 
-  GetClientIdByPhoneNumber(phoneNumber: string) {
-    throw new Error('Method not implemented.');
+  getClientDetails(clientId: string): Observable<ClientProfile> {
+    return this.http.get<ClientProfile>(`${this.baseUrl}/view-profile/${clientId}`);
+  }
+
+  updateClient(clientId: string, client: ClientProfile): Observable<void> {
+    return this.http.put<void>(`${this.baseUrl}/update-profile/${clientId}`, client);
+  }
+
+  deleteClient(clientId: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/delete-profile/${clientId}`);
   }
 }
