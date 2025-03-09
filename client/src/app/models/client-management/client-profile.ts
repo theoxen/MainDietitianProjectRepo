c62ad219055e2078debae54e30920d885bfd10b7
Dimitrizk <dikaouris@gmail.com>
2025-03-09 02:12:36 +0200

    EDIT USER FINISHED (CLIENT MANAGEMENT)

--- client/src/app/models/client-management/client-profile.ts
+++ client/src/app/models/client-management/client-profile.ts
@@ -1,9 +1,9 @@
 export interface ClientProfile {
+    userId: string;
     fullName: string; 
     phoneNumber: string;
     email: string;
     height: number;
     dietTypeName: string; 
-    gender: string; 
-    dateOfBirth: Date; 
+    
 }
