754b21beb5f1bc51af3200e81ea79dbd0070b75c
Dimitrizk <dikaouris@gmail.com>
2025-03-20 17:40:41 +0200

    test gia notes sto client history
    prepei sos na rotiso tontheodosi giati en douleflkei

--- client/src/app/pages/client-management/client-history/client-history.component.ts
+++ client/src/app/pages/client-management/client-history/client-history.component.ts
@@ -44,3 +44,43 @@ export class ClientHistoryComponent implements OnInit {
   // }
 
 }
+
+
+@Component({
+  selector: 'app-note-management',
+  template: '<p>Note Management works!</p>',
+})
+export class NoteManagementComponent implements OnInit {
+  clientId: any;
+  route: any;
+  noteService: any;
+  noteId: any;
+  clientNote: any;
+  clientNoteIsNull: boolean | undefined;
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+    this.clientId = this.route.snapshot.paramMap.get('clientId')!; // Gets the user ID from the URL (In the app.routes.ts file, the path is defined as "clients/:clientId/note")
+    this.fetchNoteForUser(this.clientId);
+    console.log("Captured Client ID in NotesforHistory:", this.clientNote.controls.note.value); // Log the note for the user
+  }
+  fetchNoteForUser(clientId: string): void {
+
+    // this.clientNote.controls.note.setValue("test"); // Set the note for the user
+
+    // Implement your logic to fetch notes for the user
+    this.noteService.fetchNoteForUser(clientId).subscribe({
+      next: (fetchedNote: { id: any; noteText: any; }) => {
+        this.noteId = fetchedNote.id; // Set the note ID
+        this.clientNote.controls.note.setValue(fetchedNote.noteText); // Set the note
+        this.clientNoteIsNull = false;
+      },
+      error: (error: any) => {
+        this.clientNoteIsNull = true;
+        // if(error.statusCode === 404) {
+        //   console.log("Note not found.");
+        // }
+      }
+    });
+    
+}
+}
